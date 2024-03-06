49b8cc6ac522ff4b87ba5ddcbd8eee559b0b46e1
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2024-03-07 02:25:42 +0530

    fixed url

--- popup.js
+++ popup.js
@@ -1,27 +1,42 @@
-async function fetchData() {
+async function fetchData(url) {
+    // url = "https://icanhazdadjoke.com/slack";
+    if (!url) {
+        console.error("No url provided");
+        return NAN;
+    }
     try {
-        const response = await fetch("https://icanhazdadjoke.com/slack");
-        const data = await response.json();
+        const response = await fetch(url);
+        const data = await response.json(url);
         const joke = data?.attachments?.[0]?.text;
         console.log(joke); // Make sure joke is not undefined
         return joke;
     } catch (error) {
-        console.error("Error fetching joke:", error);
-        return null;
+        console.error("Error calculating score:", error);
+        return NAN;
     }
 }
 
 async function getActiveTabUrl() {
-    let queryOptions = { active: true, currentWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
+    chrome.tabs.query(
+        {
+            lastFocusedWindow: true,
+            active: true,
+        },
+        async function (tabs) {
+            console.log(tabs);
+            alert(tabs[0].url);
+            console.log(tabs[0].url);
+
+            let res = await fetchData(tabs[0].url);
+            document.getElementById("productScore").innerText = res;
+        }
+    );
 }
 
 // Wait for the DOM content to be fully loaded
 document.addEventListener("DOMContentLoaded", async function () {
-    document.getElementById("productScore").innerText =
-        Math.floor(Math.random() * 11) + 90;
+    // document.getElementById("productScore").innerText =
+    //     Math.floor(Math.random() * 11) + 90;
 
-    // let url = await getActiveTabUrl();
-    // document.getElementById("productScore").innerText = url;
+    await getActiveTabUrl();
 });
